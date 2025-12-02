2b8693c6c927cc4411c724cd3a87a165c5a4df98
Thomas M <thomasjmontanaro@gmail.com>
2025-12-01 23:28:35 -0500

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '31b76914-9e27-4f67-8752-ef740a6a7762';
+export const MODAL_CLOSE_SEC = 2.5;
