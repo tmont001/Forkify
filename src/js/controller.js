60a3139f86ca744669dc1aa7df4c35c04fc68487
tmont001 <thomasjmontanaro@gmail.com>
2025-12-02 00:05:48 -0500

    Remove welcome message for practice

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,10 +124,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -136,7 +132,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
 
